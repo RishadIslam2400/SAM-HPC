7cdf678fe6b44e2923fc977a9e2d1fc6afe3b8ef
RishadIslam2400 <mds222@lehigh.edu>
2025-04-09 15:57:13 -0400

    microbenchmark script updated

--- run_microbench.sh
+++ run_microbench.sh
@@ -9,10 +9,13 @@ cd ..
 # Output file
 OUTPUT_FILE="microbench.txt"
 echo "Microbenchmark Results" > "$OUTPUT_FILE"
+echo "-----------Sequential-----------" >> "$OUTPUT_FILE"
+./build/benchmark/microbench -n "microbenchmark" -f "/top_opt_matrices_small_csc/" -k 10 >> "$OUTPUT_FILE"
 
 # Thread counts: geometric progression
+echo "-----------Parallel-----------" >> "$OUTPUT_FILE"
 for i in 2 4 8 32 64 92 128
 do
     echo "Running with $i threads..."
-    ./build/benchmark/microbench -n "microbenchmark" -f "/top_opt_matrices_small_csc/" -k 10 -t "$i" >> "$OUTPUT_FILE"
+    ./build/benchmark/microbenchmark_parallel -n "microbenchmark_parallel" -f "/top_opt_matrices_small_csc/" -k 10 -t "$i" >> "$OUTPUT_FILE"
 done
