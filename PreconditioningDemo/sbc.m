981c4b60b4a294af32e7804a38fd64b93e8ede02
RishadIslam2400 <mds222@lehigh.edu>
2025-04-09 14:53:06 -0400

    removed matlab files and data

--- PreconditioningDemo/sbc.m
+++
@@ -1,9 +0,0 @@
-function sval = sbc(x,y)
-%
-% function sval = sbc(x,y)
-%
-% implements south Dirichlet boundary condition
-
-% constant
-
-sval = 0.2+y.*(1-y.^2);
--- SAM_Spasrsity_Pattern/wbc.m
+++
@@ -1,9 +0,0 @@
-function wval = wbc(x,y)
-%
-% function wval = wbc(x,y)
-%
-% implements west Dirichlet boundary condition
-
-% constant
-
-wval = 0;%.2+y*(1-y^2);
